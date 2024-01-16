5ea767f50be9e325c554cf7ab34f78c14793514a
Student Shivam <115865754+Shivamch0@users.noreply.github.com>
2024-01-16 12:04:35 +0530

    add message in sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main(){
     int a, b, c;
     a=3;
+    cout<<"Enter the value of a :"<<a<<endl;
     b=2;
+    cout<<"Enter the value of b :"<<b<<endl;
     c=a+b;
     cout<<"the sum :"<<c<<endl;
     return 0;
