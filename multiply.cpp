18329744e061f6d8b834527ac0dcb30327afde5a
Student Shivam <115865754+Shivamch0@users.noreply.github.com>
2024-01-16 15:34:28 +0530

    added multiply

---
+++ multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b, c;
+    a=3;
+    cout<<"Enter the value of a :"<<a<<endl;
+    b=2;
+    cout<<"Enter the value of b :"<<b<<endl;
+    c=a*b;
+    cout<<"the Multiply :"<<c<<endl;
+    return 0;
+}
